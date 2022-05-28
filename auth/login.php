32f2dc5efb9dc1a1ef1cb46b581156dc33559697
EhsanIC <erfan.h.010011@gmail.com>
2022-05-28 16:39:32 +0430

    add login page

---
+++ auth/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN PAGE</title>
+</head>
+<body>
+    <h2>LOGIN PGAE</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, vero perspiciatis eum quisquam veritatis dolor deserunt ipsa culpa alias aliquid architecto, aut deleniti nam eaque nihil, amet officia ipsam repudiandae.</p>
+</body>
+</html>
